db8458f9c60c58c996cef5774c0c1d8f6eeb071d
carla-garcia <carlagg552@gmail.com>
2025-06-23 13:22:11 +0200

    TOF new code :))

--- nHits_count/functions_spills.py
+++ nHits_count/functions_spills.py
@@ -2,81 +2,133 @@ import uproot
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd  
+import json
+import awkward as ak
 
-def spill_nHitsTT(times_branch_event_arg, threshold, charge_branch_event_arg = None):
+def spill_nHitsTT(times_branch_event_arg, threshold, window, death_window, charge_branch_event = []):
     times_branch_event = np.sort(times_branch_event_arg.copy()) #just to make sure, but it is supposed to be sorted
 
     if times_branch_event[0]!=times_branch_event_arg[0]:
         print("hey Carla, the intial list was not sorted : WARNING")
 
-    if charge_branch_event_arg is not None:
-        charge_branch_event = np.array(charge_branch_event_arg).copy()
-    else:
-        charge_branch_event = None
-
     threshold_times = []
+    indices_to_delete = []
 
     i = 0
     n = len(times_branch_event)
 
     while i < n:
         time_hit = times_branch_event[i]
-        mask = (times_branch_event >= time_hit) & (times_branch_event < time_hit + 50)
-        count = mask.sum()
+        mask = (times_branch_event >= time_hit) & (times_branch_event < time_hit + window)
+
+        if len(charge_branch_event)!=0:
+            count = max(charge_branch_event[mask])
+        else:
+            count = mask.sum()
+
 
         if count > threshold:
             threshold_times.append(time_hit)
 
-            # Zero out the next 50ns after the hit window
-            mask_2 = (times_branch_event >= time_hit) & (times_branch_event < time_hit + 100)
+            # Zero out the next death window ns after the hit window
+            mask_2 = (times_branch_event >= time_hit) & (times_branch_event < time_hit + window + death_window)
 
-            times_branch_event = times_branch_event[~mask_2]
-            n = len(times_branch_event)
+            indices = np.where(mask_2)[0]
+            indices_to_delete.extend(indices.tolist())
 
-            if charge_branch_event is not None:
-                charge_branch_event = charge_branch_event[~mask_2]
-
-            # Jump ahead by 100 units
+            # Jump ahead by death window
             i += 1
-            while i < n and times_branch_event[i] < time_hit + 100:
+            while i < n and times_branch_event[i] < time_hit + window + death_window:
                 i += 1
         else:
             i += 1
 
-    return times_branch_event, threshold_times, charge_branch_event
+    return threshold_times, indices_to_delete
 
 
-def spill_ChargeTT(charge_branch_event_arg, times_branch_event_arg, threshold):
-    
-    times_branch_event = times_branch_event_arg.copy()
-    charge_branch_event = charge_branch_event_arg.copy()
-    threshold_times = []
+def repeat_spills_nHits(event_number_branch, times_branch_sorted, threshold, window, death_window):
+    threshold_times = {}
+    times_branch_modified = []
+    deleted_indices_by_event = {}
 
-    i = 0
-    n = len(times_branch_event)
+    for event in event_number_branch:
+        remaining_times = times_branch_sorted[event].copy()
+        all_thresholds = []
+        all_deleted_indices = []
+        pass_counter = 0
 
-    while i < n:
-        time_hit = times_branch_event[i]
-        mask = (times_branch_event >= time_hit) & (times_branch_event < time_hit + 50)
-        charge_max = max(charge_branch_event[mask])
-        
-        if charge_max > threshold:
-            threshold_times.append(time_hit)
+        while True:
+            new_thresholds, indices_to_delete = spill_nHitsTT(remaining_times, threshold, window, death_window)
 
-            # Zero out the next 50ns after the hit window
-            mask_2 = (times_branch_event >= time_hit) & (times_branch_event < time_hit + 100)
-            times_branch_event = times_branch_event[~mask_2]
-            charge_branch_event = charge_branch_event[~mask_2]
-            n = len(times_branch_event)
+            if not new_thresholds:
+                break
 
-            # Jump ahead by 100 units
-            i += 1
-            while i < n and times_branch_event[i] < time_hit + 100:
-                i += 1
-        else:
-            i += 1
+            # Save deleted indices relative to the original event array
+            deleted_indices = np.where(np.isin(times_branch_sorted[event], remaining_times[indices_to_delete]))[0]
+            all_deleted_indices.extend(deleted_indices.tolist())
 
-    return times_branch_event, charge_branch_event, threshold_times
+            remaining_times = np.delete(remaining_times, indices_to_delete)
+            all_thresholds.extend(new_thresholds)
+            pass_counter += 1
+
+        if pass_counter > 1:
+            print(f"Event {event}: spill_nHitsTT applied {pass_counter} times")
+
+        times_branch_modified.append(remaining_times)
+
+        if all_thresholds:
+            threshold_times[event] = all_thresholds
+
+        if all_deleted_indices:
+            deleted_indices_by_event[event] = all_deleted_indices
+
+    return times_branch_modified, threshold_times, deleted_indices_by_event
+
+
+def repeat_spills_Charge(event_number_branch, times_branch_sorted, charge_branch_sorted, window, death_window, threshold = 5000):
+    threshold_charges = {}
+    times_branch_modified_chargesTT = []
+    charge_branch_modified_chargesTT = []
+    deleted_indices_by_event = {}
+
+    for event in event_number_branch:
+        remaining_times = times_branch_sorted[event].copy()
+        remaining_charges = charge_branch_sorted[event].copy()
+        all_thresholds = []
+        all_deleted_indices = []
+        pass_counter = 0
+
+        while True:
+            new_thresholds, indices_to_delete = spill_nHitsTT(remaining_times, threshold, window, death_window, remaining_charges)
+
+            if not new_thresholds:
+                break
+
+            # Save deleted indices relative to the original event array
+            deleted_indices = np.where(np.isin(times_branch_sorted[event], remaining_times[indices_to_delete]))[0]
+            all_deleted_indices.extend(deleted_indices.tolist())
+
+            remaining_times = np.delete(remaining_times, indices_to_delete)
+            remaining_charges = np.delete(remaining_charges, indices_to_delete)
+
+            all_thresholds.extend(new_thresholds)
+            pass_counter += 1
+
+        if pass_counter > 1:
+            print(f"Event {event}: spill_nHitsTT applied {pass_counter} times")
+
+        times_branch_modified_chargesTT.append(remaining_times)
+        charge_branch_modified_chargesTT.append(remaining_charges)
+
+
+        if all_thresholds:
+            threshold_charges[event] = all_thresholds
+
+        if all_deleted_indices:
+            deleted_indices_by_event[event] = all_deleted_indices
+
+
+    return times_branch_modified_chargesTT, charge_branch_modified_chargesTT, threshold_charges, deleted_indices_by_event
 
 def plot_TotalCharge_mPMT(mpmt_id, charge):
 
@@ -102,9 +154,9 @@ def plot_TotalCharge_mPMT(mpmt_id, charge):
     plt.tight_layout()
     plt.show()
 
-def plot_TotalCharge_Time(time, charge, bin_time):
+def plot_TotalCharge_Time(time, charge, bin_time, total_time):
 
-    sum_charges = np.zeros(int(50/bin_time))
+    sum_charges = np.zeros(int(total_time/bin_time))
     div = (time-min(time))//bin_time
 
     for i, n in enumerate(div):
@@ -113,71 +165,100 @@ def plot_TotalCharge_Time(time, charge, bin_time):
     return sum_charges
 
 
-def repeat_spills_nHits(event_number_branch, times_branch_sorted, threshold = 5 ,charge_branch_sorted = None):
-    threshold_times = {}
-    times_branch_modified = []
-    charge_branch_modified = []
+def time_RMS_fun(times_event, window):
+    RMS_list = []
 
-    for event in event_number_branch:
-        remaining_times = times_branch_sorted[event].copy()
-        all_thresholds = []
-        if charge_branch_sorted is not None:
-            remaining_charges = charge_branch_sorted[event].copy()
+    for time in np.arange(0, 270000 + 1, window):
+        mask = (times_event >= time) & (times_event < time + window)
+        times_bin = times_event[mask]
+
+        N = len(times_bin)
+        if N == 0:
+            RMS_list.append(np.nan)  # or np.nan if you prefer
+            continue
+
+        mean_t = times_bin.mean()
+        RMS = np.sqrt(np.mean((times_bin - mean_t) ** 2))
+        RMS_list.append(RMS)
+
+    return RMS_list
+
+
+def read_mpmt_offsets(path):
+    with open(path) as f:
+        mcc_map = json.load(f)
+
+        d = {}
+        for k,v in zip(mcc_map.keys(), mcc_map.values()):
+            card, channel = [int(i) for i in str(int(k)/100).split(".")]
+            d[(card, channel)] = v
+
+    return d
+
+def correction_TOF(mpmt_map, mpmt_slot_branch, pmt_position, max_slot = 106, max_pos = 19):
+    lookup = np.zeros((max_slot, max_pos))
+    for (card, chan), shift in mpmt_map.items():
+        lookup[card, chan] = shift
+
+        # Hit Times Correction
+    flat_slot_ids     = ak.ravel(mpmt_slot_branch)
+    flat_pos_ids      = ak.ravel(pmt_position)
+    flat_corrections  = lookup[flat_slot_ids, flat_pos_ids]
+    corrections       = ak.unflatten(flat_corrections, ak.num(mpmt_slot_branch))
+
+    return corrections
+
+def initial_treatment(tree):
+
+    times_branch = tree["hit_pmt_calibrated_times"].array()
+    charge_branch = tree["hit_pmt_charges"].array()
+    mpmt_slot_branch = tree["hit_mpmt_slot_ids"].array()
+    pmt_position = tree["hit_pmt_position_ids"].array()
+    event_number_branch = tree["event_number"].array(library="np")
+
+    # Crear una máscara booleana por evento donde ambos valores sean >= 0
+    valid_mask = (mpmt_slot_branch >= 0) & (pmt_position >= 0)
+
+    # Aplicar la máscara a cada rama para eliminar los hits inválidos
+    times_branch_clean = times_branch[valid_mask]
+    charge_branch_clean = charge_branch[valid_mask]
+    mpmt_slot_branch_clean = mpmt_slot_branch[valid_mask]
+    pmt_position_clean = pmt_position[valid_mask]
+
+    mpmt_map = read_mpmt_offsets("/home/cgarcia_2002/nHits_count/mpmt_tof_pos1.json")
+    corrections = correction_TOF(mpmt_map, mpmt_slot_branch_clean, pmt_position_clean)
+    corrected_times = times_branch_clean - corrections
+
+    # Ordenar los tiempos por evento y obtener los índices
+    sorted_idx = ak.argsort(times_branch_clean, axis=1)
+
+    # Usar los índices para reordenar todos los branches
+    times_sorted_TOF = corrected_times[sorted_idx]
+    times_sorted = times_branch_clean[sorted_idx]
+    charges_sorted = charge_branch_clean[sorted_idx]
+    mpmt_sorted = mpmt_slot_branch_clean[sorted_idx]
+
+    # Convertir a listas de NumPy arrays
+    times_sorted_np = [np.array(evt) for evt in times_sorted]
+    times_sorted_TOF_np = [np.array(evt) for evt in times_sorted_TOF]
+    charges_sorted_np = [np.array(evt) for evt in charges_sorted]
+    mpmt_sorted_np = [np.array(evt) for evt in mpmt_sorted]
+
+    return times_sorted_np, times_sorted_TOF_np, charges_sorted_np, mpmt_sorted_np, event_number_branch
+
+def delete_indices_list(list_to_delete, indices):
+
+    new_list = []
+
+    for event, values in enumerate(list_to_delete):
+        if event in indices:
+            indices_to_delete = set(indices[event])  # convert to set for faster lookup
+
+            # Keep elements whose index is NOT in indices_to_delete
+            filtered_list = [t for i, t in enumerate(values) if i not in indices_to_delete]
         else:
-            remaining_charges = None
+            filtered_list = values.copy()
 
-        pass_counter = 0  # Count how many times the function is applied
+        new_list.append(np.array(filtered_list))
 
-        # Repeat spill_nHitsTT until no more triggers are found
-        while True:
-            remaining_times, new_triggers, remaining_charges = spill_nHitsTT(remaining_times, threshold, remaining_charges)
-            
-            if not new_triggers:
-                break
-
-            pass_counter += 1
-            all_thresholds.extend(new_triggers)
-
-        if pass_counter > 1:
-            print(f"Event {event}: spill_nHitsTT applied {pass_counter} times")
-
-        times_branch_modified.append(remaining_times)
-        if remaining_charges is not None:
-            charge_branch_modified.append(remaining_charges)
-
-        if all_thresholds:
-            threshold_times[event] = all_thresholds
-
-    return times_branch_modified, threshold_times, charge_branch_modified if charge_branch_sorted is not None else None
-
-def repeat_spills_Charge(event_number_branch, times_branch_sorted, charge_branch_sorted, threshold = 5000):
-    threshold_charges = {}
-    times_branch_modified_chargesTT = []
-    charge_branch_modified_chargesTT = []
-
-    for event in event_number_branch:
-        times_input = times_branch_sorted[event].copy()
-        charges_input = charge_branch_sorted[event].copy()
-
-        all_thresholds = []
-        pass_counter = 0
-
-        while True:
-            times_input, charges_input, threshold_charge_event = spill_ChargeTT(charges_input, times_input, threshold)
-
-            if not threshold_charge_event:
-                break
-
-            all_thresholds.extend(threshold_charge_event)
-            pass_counter += 1  
-
-        if pass_counter > 1:
-            print(f"Event {event}: spill_ChargeTT applied {pass_counter} times")
-
-        times_branch_modified_chargesTT.append(times_input)
-        charge_branch_modified_chargesTT.append(charges_input)
-
-        if all_thresholds:
-            threshold_charges[event] = all_thresholds
-
-    return times_branch_modified_chargesTT, charge_branch_modified_chargesTT, threshold_charges
+    return new_list
--- notebooks_firstTest/first_test.py
+++ notebooks_firstTest/first_test.py
@@ -4,15 +4,16 @@ import uproot
 
 # Define the relative or absolute path to the ROOT file
 # Example: '../data/myfile.root' or '/home/user/data/myfile.root'
-root_file_path = "/data/cgarcia_2002/WCTE/data/WCTE_offline_R2385S0P0.root"
+root_file_path = "/data/cgarcia_2002/WCTE/data/2384_calib_time/WCTE_offline_R2384S0P0.root" #bkg
 
 # Open the ROOT file
 with uproot.open(root_file_path) as file:
     print(file)
     print(file.classnames())
 
-    tree_v2 = file["WCTEReadoutWindows;3"]
-    tree_v3 = file["WCTEReadoutWindows;2"]
+    tree_v2 = file["WCTEReadoutWindows"]
+    tree_v3 = file["WCTEReadoutWindows;3"]
+
 
     print("tree 2", tree_v2.num_entries)  # Or tree_v3.keys()
     print("tree 3", tree_v3.num_entries)  # Or tree_v3.keys()
